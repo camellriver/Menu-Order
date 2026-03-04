df8cae734516907a181e632988208759b3d02c54
camell river <cammelriver@gmail.com>
2026-03-04 14:43:34 +0000

    Fixed changes for fires

--- combomenu.py
+++ combomenu.py
@@ -4,13 +4,13 @@ print("sandwiches: chicken $2.95, beef $9.99, tofu $1.00")
 order = input("what you want:")
 if order == "chicken":
     cost = 2.95
-    print("ok, chicken")
+    print("ok, want fries?")
 elif order == "beef":
     cost = 9.99
-    print("ok, you like beef")
+    print("ok, you want fries?")
 elif order == "tofu":
     cost = 1.00
-    print("ok, you like tofu")
+    print("ok, want fries?")
 else:
     print("we aint got that")
 
