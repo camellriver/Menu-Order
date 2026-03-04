aec5f3dc1cf4aef71c7aee3df31d4542d5981881
camell river <cammelriver@gmail.com>
2026-03-04 14:58:03 +0000

    1.00 off code

--- combomenu.py
+++ combomenu.py
@@ -4,19 +4,19 @@ print("sandwiches: chicken $2.95, beef $9.99, tofu $1.00")
 order = input("what you want:")
 if order == "chicken":
     cost = 2.95
-    print("ok, want fries?")
+    print("ok, chicken")
 elif order == "beef":
     cost = 9.99
-    print("ok, you want fries?")
+    print("ok, you like beef")
 elif order == "tofu":
     cost = 1.00
-    print("ok, want fries?")
+    print("ok, you like tofu")
 else:
     print("we aint got that")
 
 # fries code
 
-fries = input("yes or no:")
+fries = input("do you want fries? yes or no:")
 if fries == "yes":
     cost = cost + 1.50
 print("current cost: $" + str(cost))
@@ -40,3 +40,9 @@ packets = float(input("how many you want:"))
 packetcost = (packets * .25)
 cost = cost + packetcost
 print("current cost: $" + str(cost))
+
+# Coupon code, where if they ordered everything, cost is reduced by 1.00
+if order in ["chicken", "beef", "tofu"] and fries == "yes" and drink in ["super-grande", "extra-small"] and packets > 0:
+    cost = cost - 1.00
+    print("yay, you get $1.00 off")
+print("final cost: $" + str(cost))
